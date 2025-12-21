3b342b2e9e767d42ad433d7c65ab378ff5ec1878
itu-itis22-filizcan22 <115074229+itu-itis22-filizcan22@users.noreply.github.com>
2025-12-21 13:00:28 +0300

    Revert "Backend/admin
    This reverts commit d83b62c6afa0dd0c968d98bc2e23ae27802c5f03.

--- backend/src/routes/spaceRoutes.js
+++ backend/src/routes/spaceRoutes.js
@@ -2,8 +2,24 @@ const express = require('express');
 const router = express.Router();
 const spaceController = require('../controllers/spaceController');
 
-// --- PUBLIC / STUDENT ENDPOINTS ---
+//  Tümünü listele (Landing Page)
 router.get('/', spaceController.getAllSpaces);
+
+router.get('/search', spaceController.searchSpaces);
+
+router.get('/filters', spaceController.getFilterOptions);
+
 router.get('/:id', spaceController.getSpaceById);
+
+router.get('/:id/availability', spaceController.getSpaceAvailability);
+
+//  Yeni ekle (Admin Dashboard)
+router.post('/', spaceController.createSpace);
+
+// Güncelle 
+router.put('/:id', spaceController.updateSpace);
+
+//  Sil (Admin Dashboard)
+router.delete('/:id', spaceController.deleteSpace);
 
 module.exports = router;
