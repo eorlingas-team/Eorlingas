4407c53fba7c5205ac244b25c2e0b61d10486581
muratunsal <muratunsal@hotmail.com.tr>
2025-12-24 01:49:15 +0300

    fix backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3001/api';
+const baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001/api';
 
 const api = axios.create({
   baseURL,
